62aafed95b3f2d7382eee0704e96e841a0e347b4
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-05-03 22:12:26 -0500

    Add toggle for menu

---
+++ assets/js/site.js
@@ -0,0 +1,5 @@
+let mainNav = document.getElementById('navigation');
+let navToggle = document.getElementById('js-toggle');
+navTogle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
